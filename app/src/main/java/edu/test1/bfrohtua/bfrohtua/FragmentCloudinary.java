20d59ef9feab9c0ba31ac3ac85f294205dfe5ee8
viktimolog <viktimolog@gmail.com>
2017-12-08 10:27:08 +0200

    My Gallery Runtime Permission OK

--- app/src/main/java/edu/test1/bfrohtua/bfrohtua/FragmentCloudinary.java
+++ app/src/main/java/edu/test1/bfrohtua/bfrohtua/FragmentCloudinary.java
@@ -1,11 +1,18 @@
 package edu.test1.bfrohtua.bfrohtua;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.*;
+import android.content.pm.PackageManager;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -33,6 +40,7 @@ public class FragmentCloudinary extends Fragment
     private ImageView iv;
     private Controller con;
 
+    public static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 123;
     private static final int PICK_IMAGE_MULTIPLE = 78546;
     public static final String PARAM_STATUS = "PARAM_STATUS";
     public static final int STATUS_FINISH = 52178;
@@ -41,6 +49,78 @@ public class FragmentCloudinary extends Fragment
     public FragmentCloudinary()
     {
 
+    }
+
+    public boolean checkPermissionREAD_EXTERNAL_STORAGE(
+            final Context context) {
+        int currentAPIVersion = Build.VERSION.SDK_INT;
+        if (currentAPIVersion >= android.os.Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(context,
+                    Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        (Activity) context,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    showDialog("External storage", context,
+                            Manifest.permission.READ_EXTERNAL_STORAGE);
+
+                } else {
+                    ActivityCompat
+                            .requestPermissions(
+                                    (Activity) context,
+                                    new String[] { Manifest.permission.READ_EXTERNAL_STORAGE },
+                                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                }
+                return false;
+            } else {
+                return true;
+            }
+
+        } else {
+            return true;
+        }
+    }
+
+    public void showDialog(final String msg, final Context context,
+                           final String permission) {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+        alertBuilder.setCancelable(true);
+        alertBuilder.setTitle("Permission necessary");
+        alertBuilder.setMessage(msg + " permission is necessary");
+        alertBuilder.setPositiveButton(android.R.string.yes,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        ActivityCompat.requestPermissions((Activity) context,
+                                new String[] { permission },
+                                MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                    }
+                });
+        AlertDialog alert = alertBuilder.create();
+        alert.show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    // do your stuff
+                    Log.d("MyTag","onRequestPermissionsResult");
+                    Intent intent = new Intent(getActivity(), ActivityGallery.class);
+                    intent.putExtra(SERVICE_IdUser,con.getIdUser()+"");
+                    startActivity(intent);
+                }
+                else
+                    {
+                    Toast.makeText(getActivity(), "GET_ACCOUNTS Denied",
+                            Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions,
+                        grantResults);
+        }
     }
 
     class GetImage extends AsyncTask<Void, Integer, Void> {
@@ -169,9 +249,13 @@ public class FragmentCloudinary extends Fragment
             @Override
             public void onClick(View view)
             {
-                Intent intent = new Intent(getActivity(), ActivityGallery.class);
-                intent.putExtra(SERVICE_IdUser,con.getIdUser()+"");
-                startActivity(intent);
+                if (checkPermissionREAD_EXTERNAL_STORAGE(getActivity()))
+                {
+                    Log.d("MyTag","checkPermissionREAD_EXTERNAL_STORAGE OK in begin");
+                    Intent intent = new Intent(getActivity(), ActivityGallery.class);
+                    intent.putExtra(SERVICE_IdUser,con.getIdUser()+"");
+                    startActivity(intent);
+                }
             }
         });
 
@@ -194,7 +278,7 @@ public class FragmentCloudinary extends Fragment
 
                     ClipData clipData = imageReturnedIntent.getClipData();
 
-                    if (clipData == null) //choiced 1 file
+                    if (clipData == null) //selected 1 file
                     {
                         Uri uri = imageReturnedIntent.getData();
                         con.getImagesPaths().add(con.getPath(getActivity(), uri));
